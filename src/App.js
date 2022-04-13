4676a970207b4124bdc86647fb6e7b46b49f5d28
tiagokp97 <tiagopesch@gmail.com>
2022-04-13 15:40:29 -0300

    feat: Landing-page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Routes from "./routes/Routes";
 import "./App.css";
-
+import { LandingPage } from "./components/LandingPage/LandingPage";
 function App() {
   return (
     <div className="App">
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,1 @@
+import { LandingPage } from "../components/LandingPage/LandingPage";
