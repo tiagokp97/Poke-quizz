0f05112d2dd42bd5af60a67666080cd1e43522fa
CaueRafael <caue_rrds@hotmail.com>
2022-04-13 15:55:27 -0300

    feat: rotas principais do projeto

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <>
+      <Routes />
+    </>
   );
 }
 
