084199392aaf4321429d5ece5de9fba5e5c7ad61
Tiago Pesch <95288951+tiagokp97@users.noreply.github.com>
2022-04-19 16:19:59 -0300

    Update Routes.jsx

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -7,8 +7,8 @@ const Routes = () => {
   return (
     <Switch>
       <Route exact path="/">
-        {/* <LandingPage /> */}
-        <GamePage />
+      <LandingPage /> 
+     
       </Route>
       <Route exact path="/pokequiz"></Route>
 
