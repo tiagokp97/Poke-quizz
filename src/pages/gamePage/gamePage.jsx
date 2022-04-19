4048882b94cb7d538b5d376abb87965fb170e02e
CaueRafael <caue_rrds@hotmail.com>
2022-04-19 16:55:44 -0300

    feat: assets

--- src/pages/gamePage/gamePage.jsx
+++ src/pages/gamePage/gamePage.jsx
@@ -4,14 +4,17 @@ import { useState } from "react";
 import { GamePageStyles } from "./styles";
 
 import { MobileMenu } from "../../components/MobileMenu/MobileMenu";
+import logo from "../../assets/logo.png";
 const GamePage = () => {
   const [showMenu, setShowMenu] = useState(false);
   const handleClick = () => {
     setShowMenu(!showMenu);
   };
+
   return (
     <GamePageStyles>
       <Buttons className="buttonMenu" text="Menu" handleClick={handleClick} />;
+      <img src={logo} alt="logo" />
       <MobileMenu />
       {showMenu && <MobileMenu />}
     </GamePageStyles>
