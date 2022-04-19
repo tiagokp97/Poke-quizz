d8611dc7554a6ba1013224b5f48647b24e7af4d1
CaueRafael <caue_rrds@hotmail.com>
2022-04-19 16:14:09 -0300

    fix: local da gamePage

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,13 +1,14 @@
 import { Switch, Route } from "react-router";
 import { LandingPage } from "../pages/LandingPage/LandingPage";
 
-import { GamePage } from "../components/gamePage/gamePage";
+import { GamePage } from "../pages/gamePage/gamePage";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/">
-        <LandingPage />
+        {/* <LandingPage /> */}
+        <GamePage />
       </Route>
       <Route exact path="/pokequiz"></Route>
 
