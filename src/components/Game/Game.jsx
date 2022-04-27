474e406b5f44d1fc2639cf7f6519bd3b22438b27
CaueRafael <caue_rrds@hotmail.com>
2022-04-27 20:24:48 -0300

    feat: estilos input Game

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,19 +1,26 @@
 import { Container } from "./styles";
 
 import qeep from "../../assets/qeep.png";
+import { Buttons } from "../buttons/buttons";
 
 const Game = () => {
   return (
     <Container>
-      <div className="qeepBox">
-        {/* <img src={qeep} alt={"Quem é esse pokemon?"} /> */}
-        <h1> Quem é esse pokemon?</h1>
-      </div>
+      {/* <img src={qeep} alt={"Quem é esse pokemon?"} /> */}
+      <h1> Quem é esse pokemon?</h1>
+
       <div className="pokemonImg">
         <img
           src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
           alt="pikachu"
         />
+      </div>
+      <div className="inputBox">
+        É o{" "}
+        <div>
+          <input type="text" placeholder="Ex: Charmander" />
+        </div>{" "}
+        <span>►</span>
       </div>
     </Container>
   );
--- src/components/Game/styles.js
+++ src/components/Game/styles.js
@@ -5,14 +5,14 @@ export const Container = styled.div`
   width: 95%;
   height: 280px;
 
-  .qeepBox {
-    h1 {
-      font-family: "Joti One", cursive;
-      font-size: 1.5rem;
-      color: #ffcb04;
-      text-shadow: 3px 3px 0 #39569e, -1px -1px 0 #39569e, 1px -1px 0 #39569e,
-        -1px 4px 0 #39569e, 1px 1px 0 #39569e;
-    }
+  font-family: "Joti One", cursive;
+  font-size: 1rem;
+  color: #ffcb04;
+  text-shadow: 3px 3px 0 #39569e, -1px -1px 0 #39569e, 1px -1px 0 #39569e,
+    -1px 4px 0 #39569e, 1px 1px 0 #39569e;
+
+  h1 {
+    font-size: 1.5rem;
   }
 
   .pokemonImg {
@@ -21,6 +21,53 @@ export const Container = styled.div`
     img {
       height: 250px;
       filter: brightness(0);
+    }
+  }
+
+  .inputBox {
+    width: 100%;
+    display: flex;
+    font-size: 1.5rem;
+    justify-content: center;
+
+    div {
+      border-bottom: 2px solid var(--yellow);
+      margin: 0 10px;
+      border-radius: 4px;
+
+      input {
+        height: 100%;
+        background-color: #f3f3f3;
+        border: none;
+        outline: none;
+        border-radius: 4px;
+        background-color: transparent;
+
+        border-bottom: 2px solid var(--blue);
+
+        font-family: "Joti One", cursive;
+        font-size: 1rem;
+        color: #ffcb04;
+        text-shadow: 3px 3px 0 #39569e, -1px -1px 0 #39569e, 1px -1px 0 #39569e,
+          -1px 4px 0 #39569e, 1px 1px 0 #39569e;
+
+        &::placeholder {
+          text-align: center;
+        }
+      }
+    }
+    span {
+      width: 2rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border: 2px solid var(--yellow);
+      border-radius: 50%;
+      &:hover {
+        cursor: pointer;
+        border: 2px solid var(--blue);
+        color: var(--blue);
+      }
     }
   }
 
